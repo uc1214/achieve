90dfa928f6d08c8c0407b8542ec42f97ede7f6d4
Yushi Saijo <yushi.saijo@gmail.com>
2017-04-27 00:40:50 +0900

    add notification

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
+    @notification = @comment.notifications.build(user_id: @blog.user.id)
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました'}
@@ -15,6 +16,9 @@ class CommentsController < ApplicationController
             message: 'あなたの作成したブログにコメントが付きました'
             })
         end
+        Pusher.trigger("user_#{@comment.blog.user_id}_channel",'notification_created',{
+          unread_counts: Notification.where(user_id: @comment.blog.user_id, read: false).count
+          })
       else
         format.html { render :new }
       end
